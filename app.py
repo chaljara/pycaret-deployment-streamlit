bc811acac6ccd992a51dd6817748a9282c91c572
chaljara <chaljara@espol.edu.ec>
2023-11-23 21:01:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -189,9 +189,9 @@ if __name__ == '__main__':
         uploaded_file = st.file_uploader("Choose a file")
         
         if uploaded_file is not None:
-        # Can be used wherever a "file-like" object is accepted:
-        dataframe = pd.read_csv(uploaded_file)
-        st.write(dataframe)
+            # Can be used wherever a "file-like" object is accepted:
+            dataframe = pd.read_csv(uploaded_file)
+            st.write(dataframe)
 
     st.subheader('')
     
