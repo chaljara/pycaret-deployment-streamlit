4c81b6d733a745fdb3f35d1d702296377d073c3c
chaljara <chaljara@espol.edu.ec>
2023-11-29 00:19:24 -0500

    Update and rename app (1).py to app.py

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ def update_view():
             #    if uploaded_file is not None:
                     
                     
-            uploaded_file = st.file_uploader(label="Subir datos", key=str(uuid.uuid4()))#, on_change=callback_on_upload
+            uploaded_file = st.file_uploader(label="Subir datos")#, on_change=callback_on_upload
             
             if uploaded_file is not None:
                 data = pd.read_csv(uploaded_file, sep=";", encoding="UTF-8")
